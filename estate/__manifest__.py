6eb22a7e42b7c11858f20dde09bc0b031d7a1119
k3nj1g <d-nibaev@alkona.it>
2024-03-20 17:49:17 +0300

    steps 1-5

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "estate",
+    'author': "d-nibaev",
+    'category': 'Tutorials/Estate',
+    "depends": ["base", "web"],
+    "license": "LGPL-3",
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        # "views/estate_property_type_views.xml",
+        # "views/estate_property_tag_views.xml",
+        # "views/estate_property_offer_views.xml",
+        # "views/estate_menus.xml",
+    ]
+}
