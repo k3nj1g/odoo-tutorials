0f6e908c4f026d6582da906bd533e21ef24144b2
k3nj1g <d-nibaev@alkona.it>
2024-03-21 15:32:22 +0300

    Chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,15 +5,15 @@ from odoo import fields, models
 
 class EstatePropertyModel(models.Model):
     _name = "estate.property"
-    _description = "Estate Property"
+    _description = "Real Estate"
     
     name = fields.Char(required=True)
     description = fields.Char()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -26,4 +26,17 @@ class EstatePropertyModel(models.Model):
             ('east', 'East'),
             ('west', 'West'),
         ]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        required=True,
+        copy=False,
+        default='new'
     )
