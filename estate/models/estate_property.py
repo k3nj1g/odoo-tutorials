73d5dc01ffe0caa22793c90e6e0388401f61016f
k3nj1g <d-nibaev@alkona.it>
2024-03-28 17:42:08 +0300

    Chapter 9

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyModel(models.Model):
     _name = "estate.property"
@@ -13,7 +13,7 @@ class EstatePropertyModel(models.Model):
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -44,6 +44,33 @@ class EstatePropertyModel(models.Model):
     salesperson = fields.Many2one('res.users', string="Salesman", default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag')
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
+    total_area = fields.Integer(compute='_compute_total_area', string="Total Area (sqm)")
+    best_price = fields.Float(compute='_compute_best_price', string="Best Offer")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        """
+        Compute the total area based on the living area and garden area for each record.
+        """
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        """
+        Compute the best price for each record.
+        """
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price')) if record.offer_ids else 0.0
+            
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -71,3 +98,22 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', required=True, string="Partner")
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', string="Deadline")
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        """
+        Compute the date deadline based on the validity field.
+        """
+        for record in self:
+            start_date = record.create_date if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(start_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        """
+        Calculate the validity of each record based on the date deadline.
+        """
+        for offer in self:
+            start_date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.validity = (offer.date_deadline - start_date).days
